8a8c26624e59a65e5cbba4867eb0d7fa6abb5516
Lenovo <wridho246@gmail.com>
2020-06-19 22:21:15 +0700

    initial commit

---
+++ app/src/main/java/nakive/kotlin/belajar/MainFragment.kt
@@ -0,0 +1,17 @@
+package nakive.kotlin.belajar
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MainFragment:Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return LayoutInflater.from(context).inflate(R.layout.fragmen_main, container, false)
+    }
+}
